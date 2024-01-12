7504d1fd565e9c6a7ac333d5e1c0da9f8c923ec9
ikaman3 <yjhama1532@gmail.com>
2024-01-12 16:39:13 +0900

    Refactor: stack 자료구조 클래스 수정 stack.py 파일의 클래스 ArrayStack에서 pop, peek 메소드를 스택이 비어있으면 None을 반환하도록 변경

--- stack.py
+++ stack.py
@@ -1,44 +1,50 @@
 from doublylinkedlist import Node
 from doublylinkedlist import DoublyLinkedList
 
+
 class ArrayStack:
+    def __init__(self):
+        self.data = []
 
-	def __init__(self):
-		self.data = []
+    def size(self):
+        return len(self.data)
 
-	def size(self):
-		return len(self.data)
+    def isEmpty(self):
+        return self.size() == 0
 
-	def isEmpty(self):
-		return self.size() == 0
+    def push(self, item):
+        self.data.append(item)
 
-	def push(self, item):
-		self.data.append(item)
+    def pop(self):
+        if not self.isEmpty():
+            return self.data.pop()
+        else:
+            return None
 
-	def pop(self):
-		return self.data.pop()
-
-	def peek(self):
-		return self.data[-1]
+    def peek(self):
+        if not self.isEmpty():
+            return self.data[-1]
+        else:
+            return None
 
 
 class LinkedListStack:
 
-	def __init__(self):
-		self.data = DoublyLinkedList()
+    def __init__(self):
+        self.data = DoublyLinkedList()
 
-	def size(self):
-		return self.data.getLength()
+    def size(self):
+        return self.data.getLength()
 
-	def isEmpty(self):
-		return self.size() == 0
+    def isEmpty(self):
+        return self.size() == 0
 
-	def push(self, item):
-		node = Node(item)
-		self.data.insertAt(self.size() + 1, node)
+    def push(self, item):
+        node = Node(item)
+        self.data.insertAt(self.size() + 1, node)
 
-	def pop(self):
-		return self.data.popAt(self.size())
+    def pop(self):
+        return self.data.popAt(self.size())
 
-	def peek(self):
-		return self.data.getAt(self.size()).data
+    def peek(self):
+        return self.data.getAt(self.size()).data
